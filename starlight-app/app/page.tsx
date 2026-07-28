1c5938ad85944c7ee10a115c061c29a48acacaa6
awakenedarpit <awakenedarpit@gmail.com>
2026-07-28 11:59:46 +0530

    Update page.tsx

--- starlight-app/app/page.tsx
+++ starlight-app/app/page.tsx
@@ -1,10 +1,12 @@
 import Navbar from "@/components/landing/Navbar";
 
+import Background from "@/components/landing/Background";
+
 export default function Home() {
   return (
     <>
       <Navbar />
-
+<Background />
       <main
         className="container"
         style={{
