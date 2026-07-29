53d9be5182597329b7662a5f1004fe4105b99fdd
awakenedarpit <awakenedarpit@gmail.com>
2026-07-29 14:19:10 +0530

    Update Wizard.tsx
    Back Button Update

--- starlight-app/components/creator/Wizard.tsx
+++ starlight-app/components/creator/Wizard.tsx
@@ -1,16 +1,13 @@
 "use client";
 
+import { useState } from "react";
+
 import ProgressBar from "../ui/ProgressBar";
 
-import MusicStep from "./MusicStep";
-
-import PhotoStep from "./PhotoStep";
-
-import RecipientStep from "./RecipientStep";
-
-import { useState } from "react";
 import WelcomeStep from "./WelcomeStep";
-
+import RecipientStep from "./RecipientStep";
+import PhotoStep from "./PhotoStep";
+import MusicStep from "./MusicStep";
 
 export default function Wizard() {
   const [step, setStep] = useState(1);
@@ -28,21 +25,40 @@ export default function Wizard() {
         Step {step} of 8
       </div>
 
+      {/* Progress Bar */}
+      <ProgressBar step={step} total={8} />
+
       {step === 1 && (
-  <WelcomeStep onNext={() => setStep(2)} />
-)}
+        <WelcomeStep onNext={() => setStep(2)} />
+      )}
 
-{step === 2 && (
-  <RecipientStep onNext={() => setStep(3)} />
-)}
+      {step === 2 && (
+        <RecipientStep onNext={() => setStep(3)} />
+      )}
 
-{step === 3 && (
-  <PhotoStep onNext={() => setStep(4)} />
-)}
+      {step === 3 && (
+        <PhotoStep onNext={() => setStep(4)} />
+      )}
 
-{step === 4 && (
-  <MusicStep onNext={() => setStep(5)} />
-)}
-</>
-);
+      {step === 4 && (
+        <MusicStep onNext={() => setStep(5)} />
+      )}
+
+      {/* Back Button */}
+      {step > 1 && (
+        <div
+          style={{
+            marginTop: "40px",
+          }}
+        >
+          <button
+            className="secondary-btn"
+            onClick={() => setStep(step - 1)}
+          >
+            ← Back
+          </button>
+        </div>
+      )}
+    </>
+  );
 }
