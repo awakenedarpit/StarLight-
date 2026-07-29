a1d5b496e36f7f0786d522f98343e7706b5608aa
awakenedarpit <awakenedarpit@gmail.com>
2026-07-29 11:44:33 +0530

    Update Wizard.tsx
    Working Button

--- starlight-app/components/creator/Wizard.tsx
+++ starlight-app/components/creator/Wizard.tsx
@@ -1,25 +1,34 @@
 "use client";
 
 import { useState } from "react";
-import WelcomeStep from "@/components/landing/creator/WelcomeStep";
+import WelcomeStep from "./WelcomeStep";
+import RecipientStep from "./RecipientStep";
 
 export default function Wizard() {
-  const [step] = useState(1);
-  
-      return (
-              <>
-                            <div
-                                                  style={{
-                                                                                  marginBottom: "30px",
-                                                                                                                            color: "#94a3b8",
-                                                                                                                                                                                fontWeight: 600,
-                                                                                                                                                                                                                                            }}
-                                                                                                                                                                                                                                                                                                              >
-                                                                                                                                                                                                                                                                                                                                                                                        Step {step} of 8
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                        </div>
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              {step === 1 && <WelcomeStep />}
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        </>
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    );
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                }
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+  const [step, setStep] = useState(1);
+
+  return (
+    <>
+      <div
+        style={{
+          marginBottom: "20px",
+          textAlign: "center",
+          color: "#94a3b8",
+          fontWeight: 600,
+        }}
+      >
+        Step {step} of 8
+      </div>
+
+      {step === 1 && (
+        <WelcomeStep
+          onNext={() => setStep(2)}
+        />
+      )}
+
+      {step === 2 && (
+        <RecipientStep />
+      )}
+    </>
+  );
+}
